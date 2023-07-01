d154df9024e090abae251818ec7429d694cfe1dd
jesse-crypted <jesseolatunjioluwadare@gmail.comgit config --global user.name jesse-cryptedgit config --global user.email jesseolatunjioluwadare@gmail.com>
2023-07-01 03:01:53 +0100

    updated

--- 002_strings/main.dart
+++ 002_strings/main.dart
@@ -1,1 +1,13 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  String name = "Jesse";
+  String address = "Heavens place";
+  String sentence = "My name is $name and i am seated at $address";
+
+  String multilineString = '''
+I am so blessed to be yours
+i carry your seal upon my heart
+I am so blessed to be yours forever
+''';
+  print(sentence.toUpperCase());
+  print(multilineString);
+}
