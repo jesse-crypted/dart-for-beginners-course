5b946c1a062800fb1f1577abf8fa2a337efd93f6
jesse-crypted <jesseolatunjioluwadare@gmail.comgit config --global user.name jesse-cryptedgit config --global user.email jesseolatunjioluwadare@gmail.com>
2023-07-01 21:53:44 +0100

    updated

--- 005_maps/main.dart
+++ 005_maps/main.dart
@@ -1,1 +1,14 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  Map userData = {
+    'name': "Jesse",
+    'age': 19,
+    'sex': "male",
+    'height': 5.6,
+  };
+
+  print(userData['name']);
+  userData.addAll({
+    'userName': "0xJesse",
+    'state': "Ekiti",
+  });
+}
