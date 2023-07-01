289a6f27d5115d251676570573c28995faa468eb
jesse-crypted <jesseolatunjioluwadare@gmail.comgit config --global user.name jesse-cryptedgit config --global user.email jesseolatunjioluwadare@gmail.com>
2023-07-01 03:00:29 +0100

    updated file on strings

--- 003_numbers/main.dart
+++ 003_numbers/main.dart
@@ -1,1 +1,16 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  //
+  int myAge = 19;
+  int year = 2023;
+
+  //
+  double myWeight = 1.5;
+  double width = 25.6;
+
+  //
+  num num1 = 240;
+  num myHeight = 5.6;
+
+  print(year - myAge);
+  print(width / 1.5);
+}
