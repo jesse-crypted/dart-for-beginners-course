bd9bc749aedbc7785fa089f3efcc5f4f0e1e0595
jesse-crypted <jesseolatunjioluwadare@gmail.comgit config --global user.name jesse-cryptedgit config --global user.email jesseolatunjioluwadare@gmail.com>
2023-07-01 03:15:21 +0100

    updated with nullability

--- 003_numbers/main.dart
+++ 003_numbers/main.dart
@@ -1,16 +1,22 @@
 void main(List<String> args) {
   //
-  int myAge = 19;
-  int year = 2023;
+  // int myAge = 19;
+  // int year = 2023;
 
-  //
-  double myWeight = 1.5;
-  double width = 25.6;
+  // //
+  // double myWeight = 1.5;
+  // double width = 25.6;
 
-  //
-  num num1 = 240;
-  num myHeight = 5.6;
+  // //
+  // num num1 = 240;
+  // num myHeight = 5.6;
 
-  print(year - myAge);
-  print(width / 1.5);
+  // print(year - myAge);
+  // print(width / myHeight);
+  // print(myWeight / num1);
+
+  // nullability: gives the data the option to be null i.e empty ot uninitiated
+  String? value1 = "Text";
+  value1 = null;
+  print(value1.toString());
 }
