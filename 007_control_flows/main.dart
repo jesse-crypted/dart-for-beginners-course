affb1af28b62f419322ecb50f4912ab7a0341f7e
jesse-crypted <jesseolatunjioluwadare@gmail.comgit config --global user.name jesse-cryptedgit config --global user.email jesseolatunjioluwadare@gmail.com>
2023-07-04 18:03:24 +0100

    updated file

--- 007_control_flows/main.dart
+++ 007_control_flows/main.dart
@@ -1,1 +1,27 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  // String currentTrafficLight = "Black";
+
+  // if-elseIf-else statement
+  // if (currentTrafficLight == "Red") {
+  //   print("Stop🚩");
+  // } else if (currentTrafficLight == "Yellow") {
+  //   print("Slow down");
+  // } else if (currentTrafficLight == "Green") {
+  //   print("Go");
+  // } else {
+  //   print("Traffic light is broken call 911");
+  // }
+
+  // For loop
+  // for (int i = 0; i <= 10; i++) {
+  //   print("Hello Jesse $i");
+  // }
+
+  // while loop
+  int i = 0;
+  while (i < 10) {
+    print("Jesus, I want more (●'◡'●)");
+    print("There is more");
+    i++;
+  }
+}
