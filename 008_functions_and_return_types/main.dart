e0ec7be82505c0d95eebfa06875fba8a67ac6412
jesse-crypted <jesseolatunjioluwadare@gmail.comgit config --global user.name jesse-cryptedgit config --global user.email jesseolatunjioluwadare@gmail.com>
2023-07-05 22:37:16 +0100

    updated commit

--- 008_functions_and_return_types/main.dart
+++ 008_functions_and_return_types/main.dart
@@ -1,1 +1,19 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  // print(addition(5, 6));
+
+  final luckyValue = subtractFunc(b: 23, a: 54);
+  print(luckyValue + 20);
+}
+
+// int addition(num1, num2) {
+//   int add = num1 + num2;
+//   return add;
+// }
+
+int subtractFunc({
+  required int a,
+  required int b,
+}) {
+  int subtract = b - a;
+  return subtract;
+}
